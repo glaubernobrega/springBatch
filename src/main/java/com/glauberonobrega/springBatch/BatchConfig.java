0175cab87d449a4a970649eaa6431533f76162a1
Glauber Nobrega <glauberonobrega@gmail.com>
2021-02-15 20:53:14 -0300

    Segundo Projeto

--- src/main/java/com/glauberonobrega/springBatch/BatchConfig.java
+++ src/main/java/com/glauberonobrega/springBatch/BatchConfig.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -23,11 +24,13 @@ public class BatchConfig {
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
 	
+	
 	@Bean
 	public Job job() {
 		return jobBuilderFactory
 				.get("job")
 				.start(step())
+				.incrementer(new RunIdIncrementer()) 
 				.build();
 	}
 
